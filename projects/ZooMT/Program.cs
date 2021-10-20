0c0ebee06aedafd17ea851825d2d10b3743fe3f9
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-10-20 17:38:25 +0200

    Zoo Made

---
+++ projects/ZooMT/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ZooMT
+{
+    class Zoo<T>
+    {
+        public T Animal { get; set; }
+    }
+    
+    class Animal
+    {
+        
+    }
+    
+    class Mammal : Animal
+    {
+        
+    }
+
+    class Bear : Mammal
+    {
+        
+    }
+
+    class Donkey : Mammal
+    {
+        
+    }
+
+    class Lion : Mammal
+    {
+        
+    }
+
+    class Fish : Animal
+    {
+        
+    }
+
+    class Salmon : Fish
+    {
+        
+    }
+
+    class Clownfish : Fish
+    {
+        
+    }
+
+    class Student
+    {
+        
+    }
+    
+    public void AddAnimal(Animal)
+    {
+       Animal animal = new Animal();
+    }
+
+    
+}
