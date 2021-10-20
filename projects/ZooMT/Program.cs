0a565e0f0841b53cb10a23132d4b9fc3e91ba554
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-10-20 18:33:03 +0200

    Zoo Prog

--- projects/ZooMT/Program.cs
+++ projects/ZooMT/Program.cs
@@ -2,9 +2,39 @@
 
 namespace ZooMT
 {
-    class Zoo<T>
+    
+    
+    class Progam
     {
-        public T Animal { get; set; }
+        public void Main()
+        {
+            {
+                Zoo<Animal> animalZoo = new Zoo<Animal>();
+                animalZoo.AddAnimal(new Fish()); // OKAY
+                animalZoo.AddAnimal(new Clownfish()); // OKAY
+                animalZoo.AddAnimal(new Lion()); // OKAY
+                animalZoo.AddAnimal(new Donkey()); // OKAY
+            }
+            
+            
+        }
+        public void AddAnimal()
+        {
+            Animal animal = new Animal();
+        }
+
+        public void HasAnimal()
+        {
+            
+        }
+        
+        
+        
+    }
+    
+    class Zoo<T> where  T : Animal
+    {
+        
     }
     
     class Animal
@@ -52,10 +82,9 @@ namespace ZooMT
         
     }
     
-    public void AddAnimal(Animal)
-    {
-       Animal animal = new Animal();
-    }
+   
 
-    
+   
+
+
 }
