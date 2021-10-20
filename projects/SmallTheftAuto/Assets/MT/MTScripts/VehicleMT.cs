c3164a8ae0e755fc3107d4acffd651b63e9cdda9
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-10-20 09:56:02 +0200

    Start on Vehicle scripts

---
+++ projects/SmallTheftAuto/Assets/MT/MTScripts/VehicleMT.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vihicle : MonoBehaviour
+{
+    public GameObject player;
+    public CarMovementMT carMovement;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {}
+    }
+}
