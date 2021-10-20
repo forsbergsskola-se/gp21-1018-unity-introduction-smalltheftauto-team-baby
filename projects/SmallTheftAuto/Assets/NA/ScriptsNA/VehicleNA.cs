1ac325b95861d80d57331b6492abd9a14c7c79aa
BeholdtheImmolator <74328248+BeholdtheImmolator@users.noreply.github.com>
2021-10-20 11:09:46 +0200

    Entering car NA

--- projects/SmallTheftAuto/Assets/NA/ScriptsNA/VehicleNA.cs
+++ projects/SmallTheftAuto/Assets/NA/ScriptsNA/VehicleNA.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VehicleNA : MonoBehaviour{
-    public GameObject player;
-    public CarMovementNA carMovement;
+    public GameObject playerNa;
+    public CarMovementNA carMovementNa;
 
     void Update(){
-        if (Input.GetKey(KeyCode.F){
-            
+        if (Input.GetKeyDown(KeyCode.F)){
+            if (PlayerIsOutsideCar()){
+                EnterCar();
+            }
+            else {
+            LeaveCar();
+            }        
         }
+    }
+    
+    bool PlayerIsOutsideCar(){
+        return this.playerNa.activeInHierarchy;
+    }
+
+    void EnterCar(){
+        this.playerNa.SetActive(false);
+        this.carMovementNa.enabled = true;
+    }
+
+    void LeaveCar(){
+        this.playerNa.transform.position = this.transform.position;
+        this.playerNa.SetActive(true);
+        this.carMovementNa.enabled = false;
     }
 
 }
