31c03bae8819b44547fe7262028c17e526621415
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-10-21 12:00:03 +0200

    Zoo done! i hope

--- projects/ZooSO/Program.cs
+++ projects/ZooSO/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ZooSO
 {
@@ -8,72 +10,57 @@ namespace ZooSO
         
         static void Main(string[] args)
         {
-           
+            Zoo<Fish> fishZoo = new Zoo<Fish>();
+            fishZoo.AddAnimal(new Salmon());
+            fishZoo.AddAnimal(new Salmon());
+            Console.WriteLine("This should be False: "+fishZoo.HasAnimal<Clownfish>());
         }
         
         class Zoo <AnimalT> where AnimalT : Animal
         {
-            private Animal[] zooanimals = new Animal[0];
+            //private Animal[] _zooanimals = new Animal[0];
 
-            
-            public bool HasAnimal<AnimalT>()
+            private List<AnimalT> _animals = new List<AnimalT>();
+
+
+            public bool HasAnimal<TSpecies>() where TSpecies : AnimalT
             {
-                
-                // retunera något annat än true
-                return true;
+                for (int i = 0; i < _animals.Count(); i++)
+                   if (_animals[i] is TSpecies)
+                   {
+                       return true;
+                   }
+
+                return false;
             }
 
             public void AddAnimal(AnimalT animal)
             { 
-                Array.Resize<Animal>(ref zooanimals, zooanimals.Length + 1);
-                zooanimals[zooanimals.Length - 1] = animal;
+                //Array.Resize<Animal>(ref _zooanimals, _zooanimals.Length + 1);
+               // _zooanimals[^1] = animal; 
+               this._animals.Add(animal);
+
             }
         }
 
-        class Animal
-        {
+        class Animal {}
 
-        }
+        class Mammal : Animal{}
+        
+        class Bear : Mammal{}
+        
+        class Donkey : Mammal{}
+        
+        class Lion : Mammal{}
+        
+        class Fish : Animal{}
+      
+        class Salmon : Fish{}
 
-        class Mammal : Animal
-        {
-            
-        }
-
-        class Bear : Mammal
-        {
-            
-        }
-
-        class Donkey : Mammal
-        {
-
-        }
-
-        class Lion : Mammal
-        {
-
-        }
-
-        class Fish : Animal
-        {
-            
-        }
-
-        class Salmon : Fish
-        {
-            
-        }
-
-        class Clownfish : Fish
-        {
-
-        }
-
-        class Student
-        {
-
-        }
+        class Clownfish : Fish{}
+        
+        class Student{}
+       
         
         
         
