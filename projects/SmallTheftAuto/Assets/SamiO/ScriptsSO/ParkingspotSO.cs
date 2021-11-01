848da6883444b25f687368f0dd83185f6a6274ce
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-01 20:25:37 +0100

    Add PlayerHealth

---
+++ projects/SmallTheftAuto/Assets/SamiO/ScriptsSO/ParkingspotSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingspotSO : MonoBehaviour
+{
+    public bool hasCar;
+    public GameObject carPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool hasCar = true; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
