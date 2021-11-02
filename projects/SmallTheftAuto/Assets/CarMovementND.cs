a03d4106e9d2878dcaedaa40c4d6958c0a02839a
Nadia Dahlberg <nadiadahlberg@outlook.com>
2021-11-02 17:41:02 +0100

    Update to CarMovementND

--- projects/SmallTheftAuto/Assets/CarMovementND.cs
+++ projects/SmallTheftAuto/Assets/CarMovementND.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarMoveND : MonoBehaviour
+public class CarMovementND : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
