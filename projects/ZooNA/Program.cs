339792c2e3f9f58e09726751e2f045a62b74e028
BeholdtheImmolator <74328248+BeholdtheImmolator@users.noreply.github.com>
2021-11-02 17:50:35 +0100

    Finished ZooNA

--- projects/ZooNA/Program.cs
+++ projects/ZooNA/Program.cs
@@ -1,69 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ZooNA
 {
     class Program{
         static void Main(){
-            Console.WriteLine("Hello World!");
+
+            Zoo<Fish> fishZoo = new Zoo<Fish>();
+            Zoo<Mammal> mammalZoo = new Zoo<Mammal>();
+            Zoo<Donkey> donkeyZoo = new Zoo<Donkey>();
+            fishZoo.AddAnimal(new Salmon());
+            mammalZoo.AddAnimal(new Bear());
+            donkeyZoo.AddAnimal(new Donkey());
+
+            Console.WriteLine("This should be False: "+fishZoo.HasAnimal<Clownfish>());
+            Console.WriteLine("This should be True: "+mammalZoo.HasAnimal<Bear>());
         }
+
     }
 
-    public class Zoo<T> where T : Animal{
-        
+    public class Zoo<AnimalT> where AnimalT : Animal{
+
+        List<AnimalT> animals = new List<AnimalT>();
+
+        public void AddAnimal(AnimalT animal){
+            animals.Add(animal);
+        }
+
+        public bool HasAnimal<AnimalB>(){
+            foreach (var hasanimal in animals){
+                if (hasanimal is AnimalB) {
+                    return true;
+                }       
+            }
+            return false;
+        }
+
     }
 
     public class Animal{
-        public void Animal(){
-
-        }
     }
 
-        public class Mammal : Animal{
-        public void Mammal(){
-
-        }
+    public class Mammal : Animal{
     }
 
     public class Bear : Mammal{
-        public void Bear(){
-            
-        }
     }
 
     public class Donkey : Mammal{
-        public void Donkey(){
-            
-        }
     }
 
     public class Lion : Mammal{
-        public void Lion(){
-            
-        }
     }
     
     public class Fish : Animal{
-        public void Fish(){
-
-        }
     }
 
     public class Salmon : Fish{
-        public void Salmon(){
-            
-        }
     }
 
     public class Clownfish : Fish{
-        public void Clownfish(){
-            
-        }
     }
 
     public class Student{
-        public void Student(){
-            
-        }
     }
 
 }
