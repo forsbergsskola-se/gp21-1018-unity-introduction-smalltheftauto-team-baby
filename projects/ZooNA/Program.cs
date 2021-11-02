074b4cc28025f92b914be75ca9c5a1ff7a4e9273
BeholdtheImmolator <74328248+BeholdtheImmolator@users.noreply.github.com>
2021-11-02 14:54:06 +0100

    ZooNA

---
+++ projects/ZooNA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZooNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
