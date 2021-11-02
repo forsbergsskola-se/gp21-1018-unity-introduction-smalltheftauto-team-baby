ca2e96b8114f6e5bc4be861599d0535681aef44f
Nadia Dahlberg <nadiadahlberg@outlook.com>
2021-11-02 19:41:01 +0100

    Update PlayerMovementND.cs

--- projects/SmallTheftAuto/Assets/ND/ScriptsND/PlayerMovementND.cs
+++ projects/SmallTheftAuto/Assets/ND/ScriptsND/PlayerMovementND.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class PlayerMoveND : MonoBehaviour
+public class PlayerMovementND : MonoBehaviour
 {
     private void FixedUpdate()
     {
