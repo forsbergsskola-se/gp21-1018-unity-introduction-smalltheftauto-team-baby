c1cae8e179e17e266dd4a3bfd64d1db57cc84b82
Nadia Dahlberg <nadiadahlberg@outlook.com>
2021-11-03 01:12:51 +0100

    Update to VehicleND

--- projects/SmallTheftAuto/Assets/ND/ScriptsND/VehicleND.cs
+++ projects/SmallTheftAuto/Assets/ND/ScriptsND/VehicleND.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehicleND : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class VehicleND : MonoBehaviour{
+    public GameObject PlayerND;
+    public CarMovementND carMovementNd;
+    void Update(){
+        if (Input.GetKeyDown("Interact-Vehicle")){
+            if (PlayerIsOutsideCar()){
+                if(PlayerIsCloseToCar()){
+                EnterCar();
+                }
+            }
+            else {
+            LeaveCar();
+            }        
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    bool PlayerIsOutsideCar(){
+        return this.playerda.activeInHierarchy;
+    }
+    bool PlayerIsCloseToCar(){
+        return Vector3.Distance(this.playerNd.transform.position, this.transform.position) < 1;
+    }
+    void EnterCar(){
+        this.playerNd.SetActive(false);
+        this.carMovementNd.enabled = true;
+    }
+    void LeaveCar(){
+        this.playerNd.transform.position = this.transform.position;
+        this.playerNd.SetActive(true);
+        this.carMovementNd.enabled = false;
     }
 }
