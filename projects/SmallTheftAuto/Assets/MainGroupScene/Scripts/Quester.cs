870ca2eb1914567eebde1efa0274525657cbd764
BeholdtheImmolator <74328248+BeholdtheImmolator@users.noreply.github.com>
2021-11-03 20:24:47 +0100

    PhoneBox scripts

--- projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/Quester.cs
+++ projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/Quester.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quester : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class Quester : MonoBehaviour{
+    void Update(){
+        if (Input.GetButton("Interact-Phonebooth")){
+            Phonebox[] phones = FindObjectsOfType<PhoneBox>();
+            PhoneBox closestPhone = null;
+            float shortestDistance = 2;
+            foreach(PhoneBox phone in phones){
+               float phoneDistance = Vector3.Distance(this.transform.position, phone.transform.position); 
+               if (phoneDistance < shortestDistance){
+                   closestPhone = phone;
+                   shortestDistance = phoneDistance;
+               }
+            }
+            if (!(closestPhone == null)){
+                closestPhone.StartQuest(this);
+            }
+        }
     }
 }
