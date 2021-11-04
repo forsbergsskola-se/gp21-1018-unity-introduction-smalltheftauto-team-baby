7d5de99f1d7d453338259402260f17fa7ecc2470
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-11-04 14:36:03 +0100

    health bar position

--- projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/PlayerHealthBar.cs
+++ projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/PlayerHealthBar.cs
@@ -8,6 +8,12 @@ public class PlayerHealthBar : MonoBehaviour
 
     public Slider slider;
 
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
     public void SetHealth(int health)
     {
         slider.value = health;
