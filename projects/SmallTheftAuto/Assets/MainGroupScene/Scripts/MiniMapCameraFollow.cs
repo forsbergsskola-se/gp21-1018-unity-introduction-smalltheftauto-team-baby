19117b5a6a6f5254ea78983dde6bdf0e42390ce6
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-11-04 21:05:45 +0100

    Revert "CameraTweak"
    This reverts commit 5803ac616ee981fcada6ef4f498c3dfa06307b85.

--- projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/MiniMapCameraFollow.cs
+++ projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/MiniMapCameraFollow.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MiniMapCameraFollow : MonoBehaviour
 {
-    // public Transform Player;
-    // public Vector3 offset;
-    // void Start()
-    // {
-    //     transform.position = Player.position + offset;
-    //
-    //     Vector3 rot = new Vector3(90, 0, Player.eulerAngles.z);
-    //     
-    //     transform.rotation = Quaternion.Euler(rot); 
-    // }
-    
-    public float moveSpeed;
-    public GameObject targetPlayer;
-    public GameObject targetCar;
-    private Transform rigTransform; 
+    public Transform Player;
+    public Vector3 offset;
+    void Start()
+    {
+        transform.position = Player.position + offset;
 
-    void Start(){
+        Vector3 rot = new Vector3(90, Player.eulerAngles.y, 0);
         
-        rigTransform = this.transform.parent;
-    }
-
-    void FixedUpdate (){
-        if(targetPlayer == null){
-            return;
-        }
-        if(targetPlayer.activeInHierarchy){
-            rigTransform.position = Vector3.Lerp(rigTransform.position, targetPlayer.transform.position, 
-                Time.deltaTime * moveSpeed);
-        }
-        else{
-            rigTransform.position = Vector3.Lerp(rigTransform.position, targetCar.transform.position, 
-                Time.deltaTime * moveSpeed);
-        }
+        transform.rotation = Quaternion.Euler(rot); 
     }
 
     
