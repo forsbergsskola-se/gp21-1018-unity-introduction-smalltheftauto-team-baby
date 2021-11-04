f4cc883820bbe9be3c0a110acdb88815de552a8b
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-04 20:53:14 +0100

    FixFire i think

--- projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/Player.cs
+++ projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/Player.cs
@@ -14,17 +14,46 @@ public class Player : MonoBehaviour
     }
 
    
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         TakeDamage(20);
-    //     }
-    // }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Health"))
+        {
+            Healing(20);
+        } 
+        Destroy(other.gameObject);
+    }
+
+private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(20);
+        }
+    
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Healing(20);
+        }
+    
+   }
+
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
+    
         healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+      
+        }
+    }
+
+    public void Healing(int healAmount)
+    {
+        currentHealth += healAmount;
+        healthBar.SetHealth(currentHealth);
+
     }
 }
