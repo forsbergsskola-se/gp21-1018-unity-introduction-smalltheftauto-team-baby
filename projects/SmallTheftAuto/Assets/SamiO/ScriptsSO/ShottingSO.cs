b5c649c4a421d97c4bbe9c7cbe4bda4143cbe6b8
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-05 11:59:04 +0100

    we have guns ppl

--- projects/SmallTheftAuto/Assets/SamiO/ScriptsSO/ShottingSO.cs
+++ projects/SmallTheftAuto/Assets/SamiO/ScriptsSO/ShottingSO.cs
@@ -2,28 +2,108 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class ShottingSO : MonoBehaviour
 {
-  public Transform firePoint;
-  public GameObject bulletPrefab;
-
+  //Gun Stats
+  public int damage;
+  public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+  public int magazineSize, bulletsPerTap;
+  public bool allowButtonHold;
+  private int bulletsLeft, bulletShot;
   public float bulletForce = 20f;
 
 
-  private void Update()
+  private bool shooting, readyToShoot, realoding;
+  
+  //Ref
+  
+  public Transform firePoint;
+  public LayerMask whatIsEnemy;
+  public GameObject bulletPrefab;
+
+
+  public void Awake()
   {
-    if (Input.GetKeyDown(KeyCode.Mouse0))
-    {
-      Shoot();
-    }
+    bulletsLeft = magazineSize;
+    readyToShoot = true;
   }
 
-  void Shoot()
+  private void Update()
   {
+    MyInput();
+  }
+
+  private void MyInput()
+  {
+    if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+    else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+    
+    if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !realoding) Reload();
+    //shoot
+    if (readyToShoot && shooting && !realoding && bulletsLeft > 0)
+    {
+      bulletShot = bulletsPerTap;
+      Shoot();
+    }
+    
+  }
+
+  private void Shoot()
+  {
+    readyToShoot = false;
+
     GameObject bullet =  Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-    Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+    Rigidbody2D rb= bullet.GetComponent<Rigidbody2D>(); 
     
     rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+    
+    
+    bulletsLeft--;
+    bulletShot--;
+    Invoke("ResetShot", timeBetweenShooting);
+    
+    if(bulletShot > 0 && bulletsLeft > 0)
+    Invoke("Shoot", timeBetweenShots);
   }
+
+  private void ResetShot()
+  {
+    readyToShoot = true;
+  }
+  
+  
+  
+  
+  private void Reload()
+  {
+    realoding = true;
+      Invoke("ReloadFinished",reloadTime);
+  }
+
+  private void ReloadFinished()
+  {
+    bulletsLeft = magazineSize;
+    realoding = false;
+
+  }
+  
 }
+
+
+//GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+//Rigidbody rb = bullet.GetComponent(<Rigidbody>();
+
+
+
+//RaycastHit hit;
+//if (Physics.Raycast(firePoint.transform.position, firePoint.transform.up, out hit, range))
+//
+ // Debug.Log(rayHit.collider.name);
+
+  //if (rayHit.collider.CompareTag("Enemy"))
+    //Change PlayerHealthSO to work in main
+   // rayHit.collider.GetComponent<PlayerHealthSO>().TakeDamage(damage);
+//}
