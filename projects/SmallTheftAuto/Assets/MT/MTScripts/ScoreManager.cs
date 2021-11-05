1a7ebf083dee5436c53d17ad9fee92c25d9d18b5
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-11-05 18:21:46 +0100

    Score Script Done

--- projects/SmallTheftAuto/Assets/MT/MTScripts/ScoreManager.cs
+++ projects/SmallTheftAuto/Assets/MT/MTScripts/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,38 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+
+    public static ScoreManager instance;
+
     public Text scoreText;
     public Text highscoreText;
 
     int score = 0;
     private int highscore = 0;
-    
+
+
+    private void Awake()
+    {
+        instance = this;
+        
+        //ScoreManager.instance.Addpoint();
+        //  ^ add this to update ui
+    }
+
     void Start()
     {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
         scoreText.text = score.ToString() + " POINTS";
         highscoreText.text = "HIGHSCORE: " + highscore.ToString();
     }
 
     
-    void Update()
+    public void AddPoint()
     {
+        score += 1;
+        scoreText.text = score.ToString() + " POINTS";
+        if(highscore < score)
+        PlayerPrefs.SetInt("highscore", score);
         
     }
 }
