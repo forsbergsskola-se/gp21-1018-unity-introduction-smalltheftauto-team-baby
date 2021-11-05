d4919e9c651d650cd6f3d3936443c68232361ee1
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2021-11-05 22:22:40 +0100

    AcceptQuestRelocation

--- projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/PhoneBox.cs
+++ projects/SmallTheftAuto/Assets/MainGroupScene/Scripts/PhoneBox.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PhoneBox : MonoBehaviour{
+    
+    public GameObject FloatingTextQuestAcceptedPrefab; 
     public Quest StartQuest(){
         //UI accept quest, telling you you're on a quest
         Quest carHater = new Quest();
         return carHater;
+        
+        if (FloatingTextQuestAcceptedPrefab != null)
+        {
+            ShowFloatingText();
+        }
+    }
+    
+    void ShowFloatingText()
+    {
+        Instantiate(FloatingTextQuestAcceptedPrefab, transform.position, Quaternion.identity, transform);
     }
 
     public void HandInQuest(Quester quester){
